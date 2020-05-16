50accf5df677d86c3c62b5d140bcc70e249291fc
Graeme Meyer <graememeyer@hotmail.com>
2020-05-16 17:02:23 +0100

    Add files via upload

---
+++ Install-Java.ps1
@@ -0,0 +1,33 @@
+# Install-Java
+# Elevate to administrator if necessary
+If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+{
+  # Relaunch as an elevated process:
+  Start-Process powershell.exe "-File",('"{0}"' -f $MyInvocation.MyCommand.Path) -Verb RunAs
+  exit
+}
+
+$DownloadUrl = "https://javadl.oracle.com/webapps/download/AutoDL?BundleId=242060_3d5a2bb8f8d4428bbe94aed7ec7ae784"
+
+$Site = Invoke-WebRequest -UseBasicParsing "https://www.java.com/en/download/manual.jsp"
+foreach($Link in $Site.links)
+{
+    if($Link.title -match "Download Java software for Windows \(64-bit\)")
+    {
+        $FileName = $Link.innerHTML
+        Write-Output "Correct: $($Link.href)"
+        $DownloadUrl = $Link.href
+        break
+    }
+}
+$FileName = "jre.exe"
+$OutPath = "$($env:USERPROFILE)\Desktop\$($FileName)"
+
+Write-Host "Downloading from: $($DownloadUrl)"
+$ProgressPreference = 'SilentlyContinue'
+Invoke-WebRequest $DownloadUrl -UseBasicParsing -OutFile $OutPath
+Write-Host "Wrote file to $($OutPath)"
+
+Start-Process $OutPath '/s REBOOT=0 SPONSORS=0 AUTO_UPDATE=0' -wait
+Write-Output $?
+Read-Host "Complete. Press any key to exit"
