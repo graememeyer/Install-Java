ab89f0726ec9d7040f1a0a334ea17d0b4d5f2fa1
Graeme Meyer <graememeyer@hotmail.com>
2020-05-18 11:53:52 +0100

    Bug fixes - should be pretty much done now

--- Install-Java.ps1
+++ Install-Java.ps1
@@ -28,12 +28,46 @@ foreach($Link in $Site.links)
 $FileName = "jre.exe"
 $OutPath = "$($env:USERPROFILE)\Desktop\$($FileName)"
 
-Write-Host "Downloading from: $($DownloadUrl)"
+Write-Output "Downloading from: $($DownloadUrl)"
 $ProgressPreference = 'SilentlyContinue'
 Invoke-WebRequest $DownloadUrl -UseBasicParsing -OutFile $OutPath
 Write-Host "Wrote file to $($OutPath)"
 
-Write-Host "Installing the Java Runtime Environment. This can take a few minutes. Please stand by..."
+Write-Output "Installing the Java Runtime Environment. This can take a few minutes. Please stand by..."
 Start-Process $OutPath '/s REBOOT=0 SPONSORS=0 AUTO_UPDATE=0' -wait
+
+# Writes the output of the last command (E.g. an error message/status) to the output (stdout/console)
 Write-Output $?
+
+Write-Host "Installation complete."
+Write-Output "Removing the downloaded file."
+Remove-Item $OutPath
+
+Write-Output "Configuring the JAVA_HOME and system Path variables..."
+# The default Java installation path
+$Java_Default_Installation_Dir = "$($env:SystemDrive)\Program Files\Java\"
+
+# Gets the JRE version from the registry
+$JRE_Reg_Version = Get-ChildItem "HKLM:\SOFTWARE\JavaSoft\Java Runtime Environment" | Select-Object -ExpandProperty pschildname -Last 1
+
+# Checks for the install directory of that JRE version
+$JRE_Dir = Join-Path -Path $Java_Default_Installation_Dir -ChildPath "jre$($JRE_Reg_Version)"
+
+# If JRE path computed from the registry isn't valid, try to grab one that is there.
+if (-not (Test-Path $JRE_Dir)){
+    # Looks for the JRE version directory within the default installation path
+    $JRE_Dir = Get-ChildItem -Path $Java_Default_Installation_Dir -Directory -Filter 'jre*' | Select-Object -ExpandProperty FullName
+}
+
+# If the JRE path is now valid, create the JAVA_HOME system variable add it to the system PATH
+if (Test-Path $JRE_Dir){
+  try {
+    [Environment]::SetEnvironmentVariable("JAVA_HOME", $JRE_Dir, [System.EnvironmentVariableTarget]::Machine)               
+    [Environment]::SetEnvironmentVariable("Path", [Environment]::GetEnvironmentVariable('Path', [EnvironmentVariableTarget]::Machine) + "%JAVA_HOME%\bin", [EnvironmentVariableTarget]::Machine)
+  }
+  catch {
+    Write-Host "Error with the path variables"
+  }
+}
+
 Read-Host "Installation complete. Enter any key to exit"
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Install-Java
+
+Installs the Java Runtime Environment (JRE) for Windows 64 Bit and sets the JAVA_HOME and PATH system variables
